ab2a72256b3dc1a0b1c5031c5fc255c37628bb63
mlcazulo <mlcazulo@gmail.com>
2014-02-07 15:25:11 -0500

    Completed most of the basic functionality of a ClassObject.

--- src/models/ClassObject.java
+++ src/models/ClassObject.java
@@ -5,6 +5,8 @@
 
 package models;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -13,13 +15,76 @@ import java.util.Map;
  */
 public class ClassObject {
     private String name;
-    private String[] attributes;
-    private String[] operations;
+    private ArrayList<String> attributes;
+    private ArrayList<String> operations;
     private Map<ClassObject, Relationship> relationMap;
     
     public ClassObject(String name){
         this.name = name;
-        
+        attributes = new ArrayList<String>();
+        operations = new ArrayList<String>();
+        relationMap = new HashMap<ClassObject, Relationship>();
     }
+    
+    /*
+     * Adds an attribute to the class object, must provide wether it is a private
+     * attribute with the parameter boolean isPrivate.
+     */
+    public void addAttribute(String attributeName, boolean isPrivate){
+        if(isPrivate){
+            attributes.add(attributeName + " -");
+        } else{
+            attributes.add(attributeName + " +");
+        }
+    }
+    
+    public void removeAttribute(int index){
+        attributes.remove(index);
+    }
+    
+    public void removeAllAtributes(){
+        attributes.clear();
+    }
+    
+    public void addOperation(String operationName){
+        operations.add(operationName);
+    }
+    
+    public void removeOperation(int index){
+        operations.remove(index);
+    }
+    
+    public void removeAllOperations(){
+        operations.clear();
+    }
+    
+    public void addRelationship(ClassObject classObject, Relationship relationship){
+        relationMap.put(classObject, relationship);
+    }
+    
+    public void removeRelationship(ClassObject classObject){
+        relationMap.remove(classObject);
+    }
+    
+    public void removeAllRelationships(){
+        relationMap.clear();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<String> getAttributes() {
+        return attributes;
+    }
+
+    public ArrayList<String> getOperations() {
+        return operations;
+    }
+
+    public Map<ClassObject, Relationship> getRelationMap() {
+        return relationMap;
+    }
+    
     
 }
