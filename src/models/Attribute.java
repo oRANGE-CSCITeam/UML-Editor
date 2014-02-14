3de04743a0fc6c82202fb0a186d1d065146f2b2e
eddy30 <eddy30@h30.50.220.74.static.ip.windstream.net>
2014-02-14 15:11:55 -0500

    Added javaDoc to all methods in the Attribute class

--- src/models/Attribute.java
+++ src/models/Attribute.java
@@ -7,30 +7,54 @@
 package models;
 
 /**
- *
- * @author marcos
+ * Attribute class that hold all the basic information needed for an 
+ * attribute
+ * 
+ * @author oRANGE
  */
 public class Attribute {
     private String attributeName;
     private boolean type;
     
+    /**
+     * Constructor for a new attribute
+     * 
+     * @param attributeName
+     * @param type
+     */
     public Attribute(String attributeName, boolean type){
         this.attributeName = attributeName;
         this.type = type;
     }
 
+    /**
+     * Returns name of the attribute
+     * @return
+     */
     public String getAttributeName() {
         return attributeName;
     }
 
+    /**
+     * Sets a predetermined name for the attribute
+     * @param attributeName
+     */
     public void setAttributeName(String attributeName) {
         this.attributeName = attributeName;
     }
 
+    /**
+     * Returns if type is private or public
+     * @return
+     */
     public boolean isType() {
         return type;
     }
 
+    /**
+     * Sets the attribute to be private of public
+     * @param type
+     */
     public void setType(boolean type) {
         this.type = type;
     }
