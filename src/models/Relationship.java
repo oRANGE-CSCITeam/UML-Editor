d8bd9c18ddc49c5696e453d89138a36b5a7d3a4b
eddy30 <eddy30@192.168.0.102>
2014-02-13 20:24:17 -0500

    Added constructor to the Relationship class

--- src/models/Relationship.java
+++ src/models/Relationship.java
@@ -5,10 +5,43 @@
 
 package models;
 
+import java.awt.Component;
+import java.awt.Graphics;
+
 /**
  *
  * @author marcos
  */
-public class Relationship {
+public class Relationship extends Component {
+    
+    
+    
+    
+    
+    
+    /**
+     * Creates a new Relationship
+     * 
+     * @param mainClass main class of the relationship
+     * @param derivedClass the derived class of the relationship
+     * @param mainX x-coordinate of main class
+     * @param mainY y-coordinate of main class
+     * @param derivedX x-coordinate of derived class
+     * @param derivedY y-coordinate of derived class
+     * @param relaType relationship type.
+     */
+        
+    public Relationship(ClassObject mainClass, ClassObject derivedClass,
+            int mainX, int mainY, int derivedX, int derivedY, int relaType){
+        
+        
+    }
+    
+    
+    public void drawLines(Graphics g){
+        
+        
+    }
+    
     
 }
