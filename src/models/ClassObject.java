40ea90f51e5beea6b86edc2c1b4ea61afad9ab4d
eddy30 <eddy30@h30.50.220.74.static.ip.windstream.net>
2014-02-14 15:08:32 -0500

    Added javaDoc to all methods in the ClassObject class

--- src/models/ClassObject.java
+++ src/models/ClassObject.java
@@ -1,7 +1,4 @@
-/*
- *This object represents the Class Object or box for the UML editor to hold all
- *of the data of each instance of a Class Object.
- */
+
 package models;
 
 import java.awt.Color;
@@ -12,8 +9,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- *
- * @author marcos
+ * This object represents the Class Object or box for the UML editor to hold all
+ * of the data of each instance of a Class Object.
+ * 
+ * @author oRANGE
  */
 public class ClassObject {
 
@@ -30,6 +29,12 @@ public class ClassObject {
     private int widthScale = 7;
     private Color color = Color.orange;
 
+    /**
+     * Constructor for the class object
+     * @param newName - name of the newly created class
+     * @param xPos - the class x-coordinate
+     * @param yPos - the class y-coordinate
+     */
     public ClassObject(String newName, int xPos, int yPos) {
         
         attributes = new ArrayList<String>();
@@ -44,10 +49,15 @@ public class ClassObject {
         this.height = 20;
     }
 
-    /*
-     * Adds an attribute to the class object, must provide wether it is a private
-     * attribute with the parameter boolean isPrivate.
+    
+
+    /**
+     * Adds an attribute to the class object
+     * 
+     * @param attributeName - name of the attribute
+     * @param isPrivate - if attribute is private of public
      */
+    
     public void addAttribute(String attributeName, boolean isPrivate) {
         if(!attributeName.equals("")){
             if (isPrivate) {
@@ -60,16 +70,27 @@ public class ClassObject {
         }
     }
 
+    /**
+     * Removes an attribute
+     * @param index - location in the ArrayList to be removed
+     */
     public void removeAttribute(int index) {
         attributes.remove(index);
         this.width = setWidth() * widthScale + 10;
     }
 
+    /**
+     * Removes all attributes. ArrayList of Attributes becomes empty
+     */
     public void removeAllAtributes() {
         attributes.clear();
         this.width = setWidth() * widthScale + 10;
     }
 
+    /**
+     * Add an operation to the class
+     * @param operationName 
+     */
     public void addOperation(String operationName) {
         if(!operationName.equals("")){
             operations.add(operationName);
@@ -78,16 +99,28 @@ public class ClassObject {
         }
     }
 
+    /**
+     * Removes a specified operation
+     * @param index
+     */
     public void removeOperation(int index) {
         operations.remove(index);
         this.width = setWidth() * widthScale + 10;
     }
 
+    /**
+     * Removes all operations. ArrayList of operations becomes empty
+     */
     public void removeAllOperations() {
         operations.clear();
         this.width = setWidth() * widthScale + 10;
     }
 
+    /**
+     *  
+     * @param classObject
+     * @param relationship
+     */
     public void addRelationship(ClassObject classObject, Relationship relationship) {
         relationMap.put(classObject, relationship);
     }
@@ -100,14 +133,26 @@ public class ClassObject {
         relationMap.clear();
     }
 
+    /**
+     * Return name of class
+     * @return name -  the name of the class
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Returns ArrayList containing all attributes.
+     * @return attributes - holds all the attributes
+     */
     public ArrayList<String> getAttributes() {
         return attributes;
     }
 
+    /**
+     * Returns ArrayList containing all operations
+     * @return operations - hold all the operations
+     */
     public ArrayList<String> getOperations() {
         return operations;
     }
@@ -116,18 +161,34 @@ public class ClassObject {
         return relationMap;
     }
 
+    /**
+     * Returns x-coordinate of class object
+     * @return - x-coordinate
+     */
     public int getxPos() {
         return xPos;
     }
 
+    /**
+     * Returns y-coordinate of class object
+     * @return - y coordinate
+     */
     public int getyPos() {
         return yPos;
     }
 
+    /**
+     * Returns width of class object
+     * @return width of the object
+     */
     public int getWidth() {
         return width;
     }
 
+    /**
+     * Returns height of the class object
+     * @return height of the object
+     */
     public int getHeight() {
         if(operations.size() > 0 && attributes.size() == 0) {
             return height + 20;
@@ -136,24 +197,42 @@ public class ClassObject {
         } 
     }
 
+    /**
+     * Sets a predetermined x-coordinate for the object
+     * @param xPos
+     */
     public void setxPos(int xPos) {
         this.xPos = xPos;
     }
 
+    /**
+     * Sets a predetermined y-coordinate for the object
+     * @param yPos
+     */
     public void setyPos(int yPos) {
         this.yPos = yPos;
     }
 
+    /**
+     * Returns color with which the object is filled
+     * @return
+     */
     public Color getColor() {
         return color;
     }
 
+    /**
+     * Sets a color chosen by the user to fill the object
+     * @param color
+     */
     public void setColor(Color color) {
         this.color = color;
     }
 
-    
-    
+    /**
+     * Graphically displays the class object to look like a box
+     * @param g
+     */
     public void display(Graphics g) {
         //Draws the box for the entire classObject
         g.setColor(new Color(0, 0, 0, 100));
@@ -200,14 +279,26 @@ public class ClassObject {
         }
     }
 
+    /**
+     * Returns if the object is being clicked
+     * @return
+     */
     public boolean isClicked() {
         return isClicked;
     }
 
+    /**
+     * Sets the object as clicked
+     * @param bool
+     */
     public void setIsClicked(boolean bool) {
         isClicked = bool;
     }
     
+    /**
+     * Returns longest string in the attribute list
+     * @return
+     */
     public int getLongestAttribute() {
         int max = 0;
         int setMax = 0;
@@ -218,8 +309,12 @@ public class ClassObject {
             }
         return setMax;    
     }
-    
-        public int getLongestOperation() {
+
+    /**
+     * Returns longest string in the operation list
+     * @return
+     */
+    public int getLongestOperation() {
         int max = 0;
         int setMax = 0;
         for(int i = 0; i < operations.size(); i++)
@@ -230,6 +325,10 @@ public class ClassObject {
         return setMax;    
     }
     
+    /**
+     * Sets a predetermined width for the object
+     * @return
+     */
     public int setWidth(){
         int max = Math.max(name.length(), getLongestAttribute());
         return Math.max(max, getLongestOperation());
